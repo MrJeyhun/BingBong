ec69888b2a0276c1e1614036b11edc0196f9ebbc
Jeyhun Jeyhunzade <ceyhun.zade1998@gmail.com>
2020-11-15 06:21:40 +0400

    app carousel is working well

--- src/carousel.js
+++ src/carousel.js
@@ -1,5 +1,77 @@
-function test() {
-    console.log("workinwdadwadg");
+import {artists, songs, covers} from "./mockdata.js";
+
+const coversLength = document.querySelectorAll(".app__carousel ul li.app__carousel__item").length;
+export const NEXT = 1;
+export const PREV = -1;
+let startItem = 1;
+const leftpos = coversLength;
+const resetCount = coversLength;
+
+export const setCovers = () => {
+    for (let id = 1; id <= coversLength; id++) {
+        document.getElementById(`${id}`).style.backgroundImage = `url(${covers[id]})`;
+    }
 }
 
-export {test};
+export const setInfo = (id) => {
+    document.getElementById('song').textContent = `${songs[id]}`;
+    document.getElementById('artist').textContent = `${artists[id]}`;
+}
+
+//swap cover and music info
+export const swap = (direction) => {
+    if (direction == PREV) {
+        let leftItem = document.querySelector('.leftcover').getAttribute('id') - 1;
+
+        if (leftItem == 0) leftItem = coversLength;
+
+        document.querySelector('.rightcover').classList.replace('rightcover', 'backcover');
+        document.querySelector('.maincover').classList.replace('maincover', 'rightcover');
+        document.querySelector('.leftcover').classList.replace('leftcover', 'maincover');
+        document.getElementById(`${leftItem}`).classList.replace('backcover', 'leftcover')
+
+        startItem--;
+        if (startItem < 1) startItem = coversLength;
+    }
+
+    if (direction == NEXT) {
+        let position = 0;
+
+        const setPosition = (positionValue) => {
+            if (positionValue != 'leftposition') {
+                // Increment image list id
+                position++;
+
+                // If final result is greater than image count, reset position
+                if (startItem + position > resetCount) {
+                    position = 1 - startItem;
+                }
+            }
+
+            // Setting the left positioned item
+            if (positionValue == "leftposition") {
+                // Left positioned image should always be one left than main positioned image
+                position = startItem - 1;
+  
+                // Reset last image in list to left position if first image is in main position
+                if (position < 1) {
+                    position = coversLength;
+                }
+            }
+
+            return position;
+        }
+
+        document.getElementById(`${startItem}`).classList.replace('maincover', 'leftcover');
+        document.getElementById(`${startItem + setPosition()}`).classList.replace('rightcover', 'maincover');
+        document.getElementById(`${startItem + setPosition()}`).classList.replace('backcover', 'rightcover');
+        document.getElementById(`${setPosition('leftposition')}`).classList.replace('leftcover', 'backcover');
+
+        startItem++;
+        position = 0;
+        if (startItem > coversLength) startItem = 1;
+    }
+
+    setInfo(document.querySelector('.maincover').getAttribute('id'));
+}
+
---
+++ src/mockdata.js
@@ -0,0 +1,16 @@
+const artists = ["", "Pink Floyd", "Chris Isaak", "Evgeniko", "Gustavo Santaolalla"];
+
+const songs = ["", "Wish You Were Here", "Wicked Game", "Valse", "Babel"];
+
+const covers = [
+    "", 
+    "https://ia600400.us.archive.org/21/items/mbid-3d5fca37-ab04-4c74-ba0e-48f557f9485f/mbid-3d5fca37-ab04-4c74-ba0e-48f557f9485f-13694326388.jpg",
+
+    "https://ia600609.us.archive.org/29/items/mbid-412a55e2-4d85-4577-afcc-29f1cd062960/mbid-412a55e2-4d85-4577-afcc-29f1cd062960-16979283605.jpg",
+
+    "https://ia600300.us.archive.org/24/items/mbid-a623132b-c433-46a9-817a-7c0d88fd8d7f/mbid-a623132b-c433-46a9-817a-7c0d88fd8d7f-2835071589.jpg",
+
+    "https://ia800307.us.archive.org/6/items/mbid-8e7746ca-7832-41b2-a731-de5b0010f37d/mbid-8e7746ca-7832-41b2-a731-de5b0010f37d-13249592911.jpg"
+];
+
+export {artists, songs, covers};
